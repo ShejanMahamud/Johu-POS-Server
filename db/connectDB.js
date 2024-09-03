37065cde1f0bbf22d6eea664609a7197f7dacd1c
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-09-03 23:50:14 +0600

    Project setup and database setup complete

---
+++ db/connectDB.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const connectDB = async (url) => {
+  if (!url) {
+    console.log("Please provide database uri string");
+    process.exit(1);
+  }
+  try {
+    await mongoose.connect(url)
+    console.log('Database connection established!')
+  } catch (error) {
+    console.error(`Database Connection Failed: ${error.message || "An Unknown Error Occurred!"}`);
+    process.exit(1)
+  }
+};
---
+++ index.js
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import express from "express";
+import { connectDB } from "./db/connectDB";
+import { logReqRes } from "./middlewares/logReqRes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3478;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(logReqRes("log.txt"));
+
+app.get((req, res) => {
+  res.send({
+    status: 200,
+    success: true,
+    message: "Server Running...",
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error("Requested URL Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error,req, res, next) => {
+    console.log(error)
+    res.status(error.status || 500).send({
+        status: 500,
+        success: false,
+        message: error.message || "Internal Server Error"
+    })
+});
+
+//start the server using IIFE function
+(async () => {
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () =>
+          console.log("APP RUNNING ON PORT:", PORT)
+        );
+    }
+    catch(error){
+        console.error("Failed to Start the Server: ", error);
+        process.exit(1);
+    }
+})()
